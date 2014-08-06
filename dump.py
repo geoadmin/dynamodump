207ab1319a14324e5012103a31ebfab8d4fa5f8b
loicgasser <loicgasser4@gmail.com>
2014-08-06 18:09:35 +0200

    Split dump file in several smaller files

--- dump.py
+++ dump.py
@@ -10,19 +10,22 @@ import datetime
 import os 
 
 DUMP_DIR = '/var/cache/print/'
+PREFIX_NAME = 'data_'
 TABLE_NAME = 'shorturls'
 JSON_INDENT = 2
 
 t0 = time.time()
 FOLDER_NAME = datetime.datetime.fromtimestamp(t0).strftime('%d%m%Y') + '/'
-os.mkdir(DUMP_DIR + FOLDER_NAME)
+try:
+    os.mkdir(DUMP_DIR + FOLDER_NAME)
+except OSError as e:
+    print e
 
 conn = boto.connect_dynamodb()
 
 # Save Schema
 f = open(DUMP_DIR + FOLDER_NAME + 'schema.json', 'w+')
 table_desc = conn.describe_table(TABLE_NAME)
-nb_items = table_desc['Table']['ItemCount']
 f.write(json.dumps(table_desc, indent=JSON_INDENT))
 f.close()
 
@@ -30,17 +33,28 @@ f.close()
 counter = 0
 table = conn.get_table('shorturls')
 scanned_table = table.scan()
-f = open(DUMP_DIR + FOLDER_NAME + 'data.json', 'w+')
+
+# Don't write more than 1'000'000 items per file
+file_count = 1
+filename = PREFIX_NAME + str(file_count) + '.json'
+f = open(DUMP_DIR + FOLDER_NAME + filename, 'w+')
 try:
     # Needs to be a list for json.loads to work properly
     f.write('[')
     for col in scanned_table:
         json.dump(col, f, indent=JSON_INDENT)
         counter += 1
-        if counter != nb_items:
+        if counter%1000000 == 0:
+            f.write(']')
+            f.close()
+            file_count += 1
+            filename = PREFIX_NAME + str(file_count) + '.json'
+            f = open(DUMP_DIR + FOLDER_NAME + filename, 'w+')
+        else:
             f.write(',')
+except Exception as e:
+    print e
 finally:
-    f.write(']')
     f.close()
 
 tf = time.time()
--- restore.py
+++ restore.py
@@ -5,7 +5,10 @@
 import boto
 import json
 import sys
+import os
 import time
+#from multiprocessing import Pool
+
 
 DUMP_DIR = '/var/cache/print/' + sys.argv[1] + '/'
 ## Change back to shorturls!!!!!
@@ -20,11 +23,18 @@ if TABLE_NAME in conn.list_tables():
     print 'Table %s already exists, please make sure you want need to restore it.' %TABLE_NAME
     sys.exit()
 
-file_data = None
-try:
-    file_data = open(DUMP_DIR + 'data.json', 'r')
-except IOError:
-    print 'The following dump directory doesn\'t exists: %s' %DUMP_DIR
+def open_file_data(file_name):
+    try:
+        file_data = open(DUMP_DIR + file_name, 'r')
+    except IOError:
+        print 'The following dump directory doesn\'t exists: %s' %DUMP_DIR
+        sys.exit()
+    return file_data
+
+# Get data files
+files_names = os.listdir(DUMP_DIR)
+filter_data_files = lambda x: x.startswith('data_')
+files_names = filter(filter_data_files, files_names)
 
 # Recreate the table
 schema = conn.create_schema(hash_key_name='url_short',hash_key_proto_value='S')
@@ -35,7 +45,7 @@ def split_data(data, n):
     for i in xrange(0, len(data), n):
         yield data[i:i+n]
 
-def process_chunk(conn, table, chunk):
+def create_batch_lists(conn, table, chunk):
     batch_items = []
     batch_list = conn.new_batch_write_list()
     for row in chunk:
@@ -47,24 +57,40 @@ def process_chunk(conn, table, chunk):
             })
         batch_items.append(dynamo_row)
     batch_list.add_batch(table, batch_items)
+    yield batch_list
+
+def process_batch_list(conn, batch_list):
     response = conn.batch_write_item(batch_list)
     if response['UnprocessedItems']:
         # print something if dict is not empty
         print response['UnprocessedItems']
 
-try:
-    data = json.load(file_data)
-    # Load data as batches of 25 items (maximum value) and should not exceed 1Mb
-    for chunk in split_data(data, 25):
-        process_chunk(conn, table, chunk) 
-except Exception as e:
-    print 'An error occured during DB restoration'
-    print '%s' %e
-finally:
-    # Back to a regular throughput
-    table.update_throughput(25, 25)
-    if file_data:
+
+def write_data(file_name):
+    file_data = open_file_data(file_name)
+    try:
+        data = json.load(file_data)
+        # Load data as batches of 25 items (maximum value) and should not exceed 1Mb
+        for chunk in split_data(data, 25):
+            for batch_list in create_batch_lists(conn, table, chunk):
+                process_batch_list(conn, batch_list)
+    except Exception as e:
+        print 'An error occured during DB restoration'
+        print '%s' %e
         file_data.close()
+        sys.exit()
+    finally:
+        # Back to a regular throughput
+        file_data.close()
+
+##pool = Pool(processes=len(files_names))
+##pool.map(write_data, files_names)
+
+## Wait 15 secs for the table to create
+time.sleep(15)
+map(write_data, files_names)
+table.update_throughput(25, 25)
+
 
 tf = time.time()
 toff = tf - t0
